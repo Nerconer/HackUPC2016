814851288ce964cb317d8f5946499d727320d6d8
davidS <david.sola.sole@gmail.com>
2016-02-20 07:43:02 +0100

    price list

--- provesDavid.php
+++ provesDavid.php
@@ -19,7 +19,7 @@
 
 
 
-
+	$price_list = array();
 
 	$url ="http://partners.api.skyscanner.net/apiservices/pricing/v1.0";
 
@@ -42,9 +42,32 @@
 
 	$context  = stream_context_create($options);
 	$result = file_get_contents($url, false, $context);
-	var_dump($result);
-	var_dump($http_response_header);
-	var_dump($http_response_header[4]);
+	$location = $http_response_header[4] . '?apiKey=ah229592831881725379481999349293';
+	$location = str_replace("Location: ", "", $location);
+	//var_dump($location);
+	$json = file_get_contents($location);
+	$obj = json_decode($json);
+
+	//print_r($obj->Itineraries[0]);	
+	$Iti = $obj->Itineraries;
+	
+	foreach ($Iti as $priOpt) {
+		foreach ($priOpt as $o) {
+				//print_r($o);
+			foreach ($o as $a) {
+						//print_r($a);
+						$id = $a->Agents[0];
+						$price = $a->Price;
+						//print_r($id);
+						//print_r($price);
+						$price_list[$id] = $price;
+					}		
+			}
+	}
+	$price_list = array_filter($price_list);
+	//var_dump($price_list);
+
+
 
 ?>
 
