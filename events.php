38dd6c2b558718be0f587c8403589d169e7589aa
davidS <david.sola.sole@gmail.com>
2016-02-20 18:59:01 +0100

    codigos ciudades funcionando

--- events.php
+++ events.php
@@ -1,4 +1,5 @@
 <?php  
+
 	include 'import.php';
 ?>
 
@@ -22,12 +23,33 @@
 	<div id="content">
 		<?php
 			foreach ($hackathons as $hackathon) {
+				$place = $hackathon->place;
+				$place = split(',', $place);
+				$country = $place[1];
+				$place = $place[0];
+				//$code = getCode($place, $country);
+
+				$result = file_get_contents("http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/ES/EUR/en-EN/?query=".$place."&apiKey=ah229592831881725379481999349293");
+				$result = json_decode($result);
+				//var_dump($result);
+				$code = $result->Places[0]->PlaceId;
+				if($code === NULL) {
+					$result1 = file_get_contents("http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/ES/EUR/en-EN/?query=".$country."&apiKey=ah229592831881725379481999349293");
+					$code = $result1->Places[0]->PlaceId;
+				}
+
+
+
+
+				//echo "<pre>" . print_r($result,true) . "</pre>";
+				$link = 'infoHack.php?country=ES&currency=EUR&locale=es-ES&originplace=BCN-sky&destinationplace='.$code.'&outbounddate=2016-02-21';
+
 		?>
 			<div class="hackathon">
 			
 				<div class="right">
 					<img src="<?php echo $hackathon->logo; ?>"><br/>
-					<a class="btn-primary" href="infoHack.php?country=ES&currency=EUR&locale=es-ES&originplace=BCN-sky&destinationplace=PARI-sky&outbounddate=2016-02-21">Find Prices!</a>
+					<a class="btn-primary" href="<?php echo $link; ?>">Find Prices!'</a>
 				</div>
 
 				<h3><?php echo $hackathon->name; ?></h3>
@@ -40,7 +62,7 @@
 
 			</div>
 
-			<?php }?>
+			<?php } ?>
 	</div>
 </body>
 </html>
