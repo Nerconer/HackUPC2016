7c269f8d5c36dac31c0f2964950643f2c8f9020f
davidS <david.sola.sole@gmail.com>
2016-02-20 06:03:40 +0100

    working api

--- provesDavid.php
+++ provesDavid.php
@@ -1,32 +1,51 @@
 
-<?php 
+<?php
+	$apiKey = "ah229592831881725379481999349293";
+	$country = "ES";
+	$currency = "EUR";
+	$locale = "es-ES";
+	$originplace = "BCN-sky";
+	$destinationplace = "PARI-sky";
+	$outbounddate = "2016-02-21";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	$url ="http://partners.api.skyscanner.net/apiservices/pricing/v1.0";
+
+	$data = array(	'apiKey' 			=> 'ah229592831881725379481999349293', 
+					'country' 			=> 	'ES',
+					'currency' 			=> 	'EUR',
+					'locale' 			=> 	'es-ES',
+					'originplace' 		=> 	'BCN-sky',
+					'destinationplace' 	=> 	'PARI-sky',
+					'outbounddate' 		=>	'2016-02-21',
+					);
 
 	$options = array(
 	    'http' => array(
 	        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-	        'header'  => "Accept header: ‘application/json\r\n",
 	        'method'  => 'POST',
+	        'content' => http_build_query($data),
 	    ),
 	);
+
 	$context  = stream_context_create($options);
 	$result = file_get_contents($url, false, $context);
 	var_dump($result);
+	var_dump($http_response_header);
+	var_dump($http_response_header[4]);
 
-
-
-
-
-
-
-
-
-
-
-
-	$apiKey="ah229592831881725379481999349293";
-	$session = $_GET["session"];
-	$apiSessionUrl ='http://partners.api.skyscanner.net/apiservices/pricing/v1.0/'.$session.'/booking';
 ?>
 
 <!DOCTYPE html>
@@ -36,8 +55,7 @@
 </head>
 <body>
 	<div id="1">
-		<p>Holas</p>
-		<?php echo "hellooo"; ?>
+
 	</div>
 </body>
 </html>
